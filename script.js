2b0740c99d751bb9ae211994afca5ea24320a9ff
yakup <yakupsimseke@hotmail.com>
2023-06-30 00:00:39 +0300

    error

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const qrText = document.querySelector("#qrT")
 const imgBox = document.querySelector("#imgBox")
 
 QRgenerate = () =>{
-  if(qrText.value.lenght >0){
+  if(qrText.value.length > 0  ){
       qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrText.value
-
+      console.log("Başarılı");
     }else{
-      qrText.classList.add('error')
+      qrText.classList.add("error")
       setTimeout(()=>{
-        qrText.classList.remove('error')
+        qrText.classList.remove("error")
       },1000)
     }
  
